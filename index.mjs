19f7bb5c83b91b46cb073784ff0015cce704237f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-04-24 01:35:23 +0200

    Remove window.unload
    https://developer.mozilla.org/en-US/docs/web/api/window/unload_event

--- index.mjs
+++ index.mjs
@@ -1,18 +1,7 @@
 import { VerovioToolkit } from './verovio-toolkit.mjs';
-import { emscriptenProxy } from './emscripten-proxy.mjs';
 import VerovioModule from './wasm-module.mjs';
 
 export {
     VerovioModule,
     VerovioToolkit,
 };
-
-// If the window object is defined (if we are not within a WebWorker)...
-if ((typeof window !== 'undefined') && window.addEventListener) {
-    // Add a listener that will delete the object (if necessary) when the page is closed
-    window.addEventListener('unload', function () {
-        for (let i = 0; i < VerovioToolkit.instances.length; i++) {
-            emscriptenProxy.destructor(VerovioToolkit.instances[i]);
-        }
-    });
-}

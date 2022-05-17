922068083e67af49cfd19499c80f04cb8038e39a
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-05-17 22:26:05 +0200

    Add legacy-wasm.js

---
+++ src/legacy-wasm.js
@@ -0,0 +1,7 @@
+import Module from '../modules/verovio-toolkit-wasm.js'
+import { VerovioToolkit } from './verovio-toolkit.js';
+
+export default {
+    module: Module,
+    toolkit: VerovioToolkit,
+}
